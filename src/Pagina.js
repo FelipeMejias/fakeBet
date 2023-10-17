e7f25fea7bee49d486a6ce71b3410582617b5274
Felipe Mejias <felipe.mejias50@gmail.com>
2023-10-17 14:26:29 -0300

    fixxx

--- src/Pagina.js
+++ src/Pagina.js
@@ -3,22 +3,44 @@ import logobet from './fakeBET.png'
 import logofutbase from './logofutbase.png'
 import { useId } from 'react';
 import { useResolvedPath } from 'react-router-dom';
-const listaTimes=['amg','cap','cam','bah','bot','bra','cor','ctb','cru','cui','fla','flu','for','goi','gre','int','pal','san','sao','vas',
-'abc','ago','ava','botsp','cea','cha','crb','cri','gua','itu','juv','lon','mir','nov','pon','sam','spo','tom','vil','vit',
-'ars','ast','bou','bre','bri','bur','che','cry','eve','ful','liv','lut','mau','mac','new','not','she','tot','wes','wol',
-'ala','alm','atb','atm','bar','bet','cad','cel','get','gir','gra','las','mal','osa','ray','rem','res','sev','val','vill',
-'ata','bol','cag','emp','fio','fro','gen','inte','juve','laz','lec','mil','mon','nap','rom','sal','sas','tor','udi','ver']
-const listaNomes=['América MG','Athlético PR','Atlético MG','Bahia','Botafogo','Bragantino','Corinthians','Coritiba','Cruzeiro','Cuiabá','Flamengo','Fluminense','Fortaleza','Goiás','Grêmio','Internacional','Palmeiras','Santos','São Paulo','Vasco da Gama',
-'ABC','Atlético GO','Avaí','Botafogo SP','Ceará','Chapecoense','CRB','Criciúma','Guarani','Ituano','Juventude','Londrina','Mirassol','Novorizontino','Ponte Preta','Sampaio Corrêa','Sport','Tombense','Vila Nova','Vitória',
-'Arsenal','Aston Villa','Bournemouth','Brentford','Brighton','Burnley','Chelsea','Crystal Palace','Everton','Fulham','Liverpool','Luton Town','Manchester United','Manchester City','Newcastle','Nottingham Forest','Sheffield United','Tottenham','West Ham','Wolverhampton',
-'Alavés','Almeria','Athletico Bilbao','Athletico Madrid','Barcelona','Bétis','Cádiz','Celta de Vigo','Getafe','Girona','Granada','Las Palmas','Mallorca','Osasuna','Rayo Vallecano','Real Madrid','Real Sociedad','Sevilla','Valencia','Villareal',
-'Atalanta','Bologna','Cagliari','Empoli','Fiorentina','Frosinone','Genoa','Inter','Juventus','Lazio','Lecce','Milan','Monza','Napoli','Roma','Salernitana','Sassuolo','Torino','Udinese','Verona'
-]
+
 export default function Pagina(){
+  const paths=['bra1','ing1','esp1','ita1','bra2']
+  const contextBra1={
+    listaNomes:['América MG','Athlético PR','Atlético MG','Bahia','Botafogo','Bragantino','Corinthians','Coritiba','Cruzeiro','Cuiabá','Flamengo','Fluminense','Fortaleza','Goiás','Grêmio','Internacional','Palmeiras','Santos','São Paulo','Vasco da Gama'],
+    listaTimes:['amg','cap','cam','bah','bot','bra','cor','ctb','cru','cui','fla','flu','for','goi','gre','int','pal','san','sao','vas'],
+    path:'bra1'
+  }
+  const contextIng1={
+    listaNomes:['Arsenal','Aston Villa','Bournemouth','Brentford','Brighton','Burnley','Chelsea','Crystal Palace','Everton','Fulham','Liverpool','Luton Town','Manchester United','Manchester City','Newcastle','Nottingham Forest','Sheffield United','Tottenham','West Ham','Wolverhampton'],
+    listaTimes:['ars','ast','bou','bre','bri','bur','che','cry','eve','ful','liv','lut','mau','mac','new','not','she','tot','wes','wol'],
+    path:'ing1'
+  }
+  const contextEsp1={
+    listaNomes:['Alavés','Almeria','Athletico Bilbao','Athletico Madrid','Barcelona','Bétis','Cádiz','Celta de Vigo','Getafe','Girona','Granada','Las Palmas','Mallorca','Osasuna','Rayo Vallecano','Real Madrid','Real Sociedad','Sevilla','Valencia','Villareal'],
+    listaTimes:['ala','alm','atb','atm','bar','bet','cad','cel','get','gir','gra','las','mal','osa','ray','rem','res','sev','val','vil'],
+    path:'esp1'
+  }
+    const contextIta1={
+      listaNomes:['Atalanta','Bologna','Cagliari','Empoli','Fiorentina','Frosinone','Genoa','Inter','Juventus','Lazio','Lecce','Milan','Monza','Napoli','Roma','Salernitana','Sassuolo','Torino','Udinese','Verona'],
+      listaTimes:['ata','bol','cag','emp','fio','fro','gen','int','juv','laz','lec','mil','mon','nap','rom','sal','sas','tor','udi','ver'],
+      path:'ita1'
+    }
+    const contextBra2={
+      listaNomes:['ABC','Atlético GO','Avaí','Botafogo SP','Ceará','Chapecoense','CRB','Criciúma','Guarani','Ituano','Juventude','Londrina','Mirassol','Novorizontino','Ponte Preta','Sampaio Corrêa','Sport','Tombense','Vila Nova','Vitória'],
+      listaTimes:['abc','ago','ava','bot','cea','cha','crb','cri','gua','itu','juv','lon','mir','nov','pon','sam','spo','tom','vil','vit'],
+      path:'bra2'
+    }
+  const contexts=[
+    contextBra1,contextIng1,contextEsp1,contextIta1,contextBra2
+  ]
     const path=useResolvedPath()
     const list=path.pathname.split('/')[1].split('-')
-    const time1=listaNomes[listaTimes.indexOf(list[0])]
-    const time2=listaNomes[listaTimes.indexOf(list[1])]
+    const pathEscolhido=list[0]
+    const context=contexts[paths.indexOf(pathEscolhido)]
+    const {listaNomes,listaTimes}=context
+    const time1=listaNomes[listaTimes.indexOf(list[1])]
+    const time2=listaNomes[listaTimes.indexOf(list[2])]
     return (
         <Tudo>
         <Header>
